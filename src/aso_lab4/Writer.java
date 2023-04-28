433b0c5d7116769f382eaa67147cdc1cf4237095
Qrroll <dainsureivu@gmail.com>
2023-04-28 18:20:13 +0300

    edit/Writer

--- src/aso_lab4/Writer.java
+++ src/aso_lab4/Writer.java
@@ -1,5 +1,9 @@
 package aso_lab4;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
@@ -12,12 +16,14 @@ public class Writer extends Thread {
     private final ArrayList<String> titles;
     private final Random random;
 
+    private static final Logger logger = LogManager.getLogger(Writer.class);
     public Writer(Library _library, String name) {
         library = _library;
         written = new ArrayList<>();
         setName(name);
         random = new Random();
         titles = new ArrayList<>();
+        Configurator.initialize(null, "src/resource/log4j2.xml");
     }
 
     @Override
@@ -27,6 +33,7 @@ public class Writer extends Thread {
                 synchronized (this) {
                     sleep(random.nextInt(50));
                     library.writeLock.lock();
+                    logger.info("writeLock - locked.");
                     String filePath = "src/resource/books.csv";
                     try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
                         String line;
@@ -34,6 +41,7 @@ public class Writer extends Thread {
                             titles.add(line);
                         }
                     }
+                    logger.info("file: books.csv - read.");
                     //System.out.println(getName() + ": titles - " + getCurrentBooksString());
                     ArrayList<Book> addedLib = library.getAddedBooks();
                     if (addedLib.size() != library.getMaxBooks()) {
@@ -44,17 +52,22 @@ public class Writer extends Thread {
                         tempInt = tempInt / 100;
                         String title = titles.get(tempInt);
                         Book newBook = new Book(title);
-                        System.out.printf("%s: write book - %s\n", getName(), title);
+                        //System.out.printf("%s: write book - %s\n", getName(), title);
+                        logger.info("write book - {}", title);
                         library.addBook(newBook);
                         library.writeLock.unlock();
+                        logger.info("writeLock - unlocked.");
                     } else {
                         library.writeLock.unlock();
+                        logger.info("writeLock - unlocked.");
+                        logger.info("loop - break.");
                         break;
                     }
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
+            logger.error("catch exception.");
         }
     }
 }

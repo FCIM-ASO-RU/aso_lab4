359a50f3922c8307312acd332c8148613cdfa3b0
Qrroll <dainsureivu@gmail.com>
2023-04-28 16:14:00 +0300

    complete/Library

--- src/aso_lab4/Library.java
+++ src/aso_lab4/Library.java
@@ -9,8 +9,9 @@ public class Library {
     final Writer[] writers;
     final Reader[] readers;
     
-    public final int max_books;
-    public final ArrayList<Book> books;
+    private final int max_books;
+    private final ArrayList<Book> currentBooks;
+    private final ArrayList<Book> addedBooks;
     public final Lock writeLock;
     public final Lock readLock;
     
@@ -19,14 +20,47 @@ public class Library {
         readers = new Reader[_readers];
         
         max_books = _max_books;
-        books = new ArrayList<>();
+        currentBooks = new ArrayList<>();
+        addedBooks = new ArrayList<>();
         
         ReentrantReadWriteLock rwl = new ReentrantReadWriteLock(true);
         writeLock = rwl.writeLock();
         readLock = rwl.readLock();
         
-        for(int i = 0; i < _writers; i++)
+        for(int i = 0; i < _writers; i++) {
             writers[i] = new Writer(this, "Writer "+(i+1));
+            writers[i].start();
+        }
+        for(int i = 0; i < _readers; i++) {
+            readers[i] = new Reader(this, "Reader "+(i+1));
+            readers[i].start();
+        }
     }
-    
+    public synchronized void addBook(Book _book){
+        currentBooks.add(_book);
+        addedBooks.add(_book);
+        System.out.printf("Library current books: %s \n", getCurrentBooksString());
+    }
+    public synchronized Book getBook(int _index){
+        try {
+            return currentBooks.get(_index);
+        } finally {
+            currentBooks.remove(_index);
+            System.out.printf("Library current books: %s \n", getCurrentBooksString());
+        }
+    }
+    public synchronized void putBook(Book _book){
+        currentBooks.add(_book);
+        System.out.printf("Library current books: %s \n", getCurrentBooksString());
+    }
+    public synchronized String getCurrentBooksString() {
+        String currentTitles = "";
+        for(Book book : currentBooks) {
+            currentTitles = currentTitles + " " + book.getTitle();
+        }
+        return currentTitles;
+    }
+    public synchronized ArrayList<Book> getCurrentBooks(){ return currentBooks; }
+    public synchronized ArrayList<Book> getAddedBooks(){ return addedBooks; }
+    public synchronized int getMaxBooks(){ return max_books; }
 }

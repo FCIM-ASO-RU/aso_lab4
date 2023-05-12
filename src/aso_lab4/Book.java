f10ff95f07fd46e02271d77f5dbd51ff2a59c4d5
Vadim Shvets <vadimshvets@gmail.com>
2023-05-12 16:49:11 +0300

    added book

--- src/aso_lab4/Book.java
+++ src/aso_lab4/Book.java
@@ -1,11 +1,14 @@
 package aso_lab4;
 
 public class Book {
-    
+
     public final String title;
-    
-    public Book(String _title) {
+
+    public final int pages;
+
+    public Book(String _title,int _pages) {
         title = _title;
+        pages = _pages;
     }
-    
+
 }

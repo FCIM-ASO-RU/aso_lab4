59357016f34bce20e867e935e84f46d3c689f455
Vadim Shvets <vadimshvets@gmail.com>
2023-05-12 16:53:57 +0300

    added main

--- src/aso_lab4/Main.java
+++ src/aso_lab4/Main.java
@@ -3,8 +3,11 @@ package aso_lab4;
 public class Main {
 
     public static void main(String[] args) {
-        Library library = new Library(0, 0, 0); // указываете в соответствии с вариантом
-        
+        int x = 10;//3 + 7 Вадим 8
+        int y = 11;//3 + 8; Дима 7
+        int z = 13;//10 + 3 Ярик 10
+        final Library library = new Library(x, y, z); // указываете в соответствии с вариантом
+        library.Start();
     }
-    
+
 }
